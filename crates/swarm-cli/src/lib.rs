f005365cae6a034e25beff31669ac727d3d409b8
MousaXD <101021011+MousaXD@users.noreply.github.com>
2026-08-18 03:46:01 +0400

    runtime: remove temporary lint workaround

--- crates/swarm-cli/src/lib.rs
+++ crates/swarm-cli/src/lib.rs
@@ -1,9 +1,4 @@
 pub mod authority_permit;
 pub mod migration;
-#[allow(
-    unused_imports,
-    clippy::vec_init_then_push,
-    clippy::if_same_then_else
-)]
 pub mod runtime_installer;
 pub mod runtime_layout;
